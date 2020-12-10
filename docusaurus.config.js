3f9b9235efd6b94c64aa2a7bdaaccebc4b4005a7
Fernando Trigoso <fertrig@gmail.com>
2020-12-10 15:00:46 -0500

    monarch favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/monarch_favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
