16d6ffa629be52a53b989ed3ebd642aa648378db
Fernando Trigoso <ftrigoso@dropsource.com>
2021-08-30 17:27:15 -0400

    newsletter section styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -174,13 +174,14 @@ function Home() {
             </div>
           </div>
         </section>
-        <section className={clsx(styles.valuePropSection)}>
+        <section className={clsx(styles.valuePropSection, styles.newsletterSection)}>
           <div className="container">
             <h2>Join our newsletter</h2>
             <p>Sign up to receive low frequency emails on the latest Monarch updates, features and news!</p>
             <div>
               <a
                 className={clsx('button button--outline button--primary button--lg')}
+                target="_blank"
                 href="https://docs.google.com/forms/d/e/1FAIpQLSfD9L92rymZnf-z18hWj0gyqrnBFNcc7U3YB3QyiI0_3PMxhQ/viewform?usp=sf_link">
                 Sign Up Now</a>
             </div>
