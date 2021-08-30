543d1393cc618423cd26aa71dc430f9a91114e4e
Fernando Trigoso <ftrigoso@dropsource.com>
2021-08-30 16:01:01 -0400

    newsletter section landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -174,6 +174,18 @@ function Home() {
             </div>
           </div>
         </section>
+        <section className={clsx(styles.valuePropSection)}>
+          <div className="container">
+            <h2>Join our newsletter</h2>
+            <p>Sign up to receive low frequency emails on the latest Monarch updates, features and news!</p>
+            <div>
+              <a
+                className={clsx('button button--outline button--primary button--lg')}
+                href="https://docs.google.com/forms/d/e/1FAIpQLSfD9L92rymZnf-z18hWj0gyqrnBFNcc7U3YB3QyiI0_3PMxhQ/viewform?usp=sf_link">
+                Sign Up Now</a>
+            </div>
+          </div>
+        </section>
       </main>
     </Layout>
   );
